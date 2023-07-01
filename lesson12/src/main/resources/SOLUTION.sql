4a358ce186a88df9f3373344eddc9cafcaa9d7cb
Khamroev <khamroevjs@gmail.com>
2023-07-01 19:16:11 +0300

    lesson12 fix

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,10 +1,3 @@
-alter table mark drop constraint mark_student_id_fkey;
-alter table payment drop constraint payment_student_id_fkey;
-alter table payment drop constraint payment_type_id_fkey;
-alter table mark add constraint mark_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
-alter table payment add constraint payment_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
-alter table payment add constraint payment_type_id_fkey foreign key (type_id) references paymenttype(id) on delete cascade;
-
 delete from student where id in (select distinct student_id from mark join subject on (subject.id = mark.subject_id) where grade >= 4);
 delete from student where id in (select distinct student_id from student join mark on student.id = mark.student_id where mark.mark < 4);
 delete from paymenttype where name = 'DAILY';
