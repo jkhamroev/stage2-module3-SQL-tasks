244c38801965d52d5c3437d259bf13651afe84f1
Khamroev <khamroevjs@gmail.com>
2023-07-01 19:41:16 +0300

    lesson7 fix 2

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -1,4 +1,5 @@
 select * from mark where mark > 6 order by mark desc;
-select * from payment where amount < 300 order by amount;
-select * from paymenttype order by name desc;
-select student.* from student join payment on student.id = payment.student_id where payment.amount > 1000 order by student.birthday;
+select * from payment where amount < 300 order by amount asc;
+select * from paymenttype order by name asc;
+select * from student order by name desc;
+select student.* from student join payment on student.id = payment.student_id where payment.amount > 1000 order by student.birthday asc;
