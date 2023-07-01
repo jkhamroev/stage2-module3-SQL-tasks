784a2c7eba45b590bc0eff81befbd29c3df9d4aa
Khamroev <khamroevjs@gmail.com>
2023-07-01 18:40:09 +0300

    lesson5 fix

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,6 +2,6 @@ select * from payment where amount >= 500;
 select * from student where DATEADD('YEAR', -20, CURRENT_DATE) > birthday;
 select * from student where groupnumber = 10 and DATEADD('YEAR', -20, CURRENT_DATE) < birthday;
 select * from student where name like 'Mike %' or groupnumber in(4, 5, 6);
-select * from payment where DATEADD('MONTH', -8, CURRENT_DATE) <= payment_date;
+select * from payment where DATEADD('MONTH', -8, PARSEDATETIME('20230115', 'yyyyMMdd')) <= payment_date;
 select * from student where name like 'A%';
-select * from student where (name = 'Roxi' and groupnumber = 4) or (name = 'Tallie' and groupnumber = 9);
+select * from student where (name like 'Roxi %' and groupnumber = 4) or (name like 'Tallie %' and groupnumber = 9);
